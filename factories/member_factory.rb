fbf61b9f0049374da21e0a84922378912231682c
sploiber <joelgottlieb@yahoo.com>
2012-09-18 10:05:42 -0400

    Updating factory

--- factories/member_factory.rb
+++ factories/member_factory.rb
@@ -2,13 +2,15 @@ FactoryGirl.define do
   factory :member do
     first_name 'Kyle'
     last_name 'Korver'
-    address '1715 Amwell Road'
+    address_1 '1715 Amwell Road'
+    address_2 '#1'
     city 'Somerset'
     state 'NJ'
     zip '08873'
     home_phone '7322460009'
     work_phone '7322490110'
-    email 'kyle@njaimh.org'
+    email_1 'kyle@njaimh.org'
+    email_2 'korver@njaimh.org'
     agency 'NJAIMH'
   end
 end
