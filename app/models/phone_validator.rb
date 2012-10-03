30b4a04d69666dc8b2e5cbb9823b9b688684436e
sploiber <joelgottlieb@yahoo.com>
2012-10-03 08:16:20 -0400

    Fixing too stringent check

--- app/models/phone_validator.rb
+++ app/models/phone_validator.rb
@@ -1,8 +1,9 @@
 class PhoneValidator < ActiveModel::EachValidator
+  # It is OK to have a null phone, but nothing else is OK
   def validate_each(record, attribute, value)
     record.errors[attribute] << (
       options[:message] || "is not a valid phone number"
     ) unless
-      value =~ /\(\d{3}\)\d{3}\-\d{4}/i
+      (value == "") || (value =~ /\(\d{3}\)\s+\d{3}\-\d{4}/i)
   end
 end
